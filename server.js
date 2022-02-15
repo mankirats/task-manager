d660b79c77e47fa7f66378c73c1f6836e3ea67a3
Mankirat <mankirat95@gmail.com>
2022-02-15 17:39:28 +0530

    User model added

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get("*", (req, res) => {
+    res.status(400).send({
+        status: 400,
+        message: "Oops!, I think you are lost.",
+    });
+});
+
+app.listen(port, () => {
+    console.log("App is running on port -", port);
+});
