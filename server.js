de2823e6550bf254fb9b12d73a86be9f9664442a
Mankirat <mankirat95@gmail.com>
2022-02-15 21:58:26 +0530

    Task model integrated

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const User = require("./src/models/user.js");
+const Task = require("./src/models/task");
+const res = require("express/lib/response");
 require("./src/db/mongoose");
+
 app.use(express.json());
 
 app.get("/api/v1/user", (req, res) => {
@@ -17,16 +20,49 @@ app.post("/api/v1/user", (req, res) => {
     newUser
         .save()
         .then(() => {
-            res.status(200).send({
-                status: 200,
+            res.status(201).send({
+                status: 201,
                 message: "New user Added",
-                data: res.send(newUser),
+                data: newUser,
             });
         })
-        .catch((err) => {
+        .catch((e) => {
             res.status(400).send({
                 status: 400,
-                message: `${err}`,
+                message: `${e}`,
+            });
+        });
+});
+
+app.get("/api/v1/task", (req, res) => {
+    const allTask = Task.find({}, function (err, result) {
+        if (err) {
+            return err;
+        } else {
+            return result;
+        }
+    });
+    res.status(200).send({
+        status: 200,
+        message: allTask,
+    });
+});
+
+app.post("/api/v1/task", (req, res) => {
+    const newTask = new Task(req.body);
+    newTask
+        .save()
+        .then(() => {
+            res.status(201).send({
+                status: 201,
+                message: "Task created successfully",
+                data: newTask,
+            });
+        })
+        .catch((e) => {
+            res.status(400).send({
+                status: 400,
+                message: e,
             });
         });
 });
