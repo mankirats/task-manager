83ea9ffd12150483d815cdfdbadfdd67d5aee11e
Mankirat <mankirat95@gmail.com>
2022-02-23 20:36:00 +0530

    Password authentication added in the user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-
-const User = mongoose.model("User", {
+const bcrypt = require("bcryptjs");
+const { use } = require("express/lib/application");
+const userSchema = mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -21,5 +22,15 @@ const User = mongoose.model("User", {
         },
     },
 });
+
+userSchema.pre("save", async function (next) {
+    const user = this;
+    if (user.isModified("password")) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
+});
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
