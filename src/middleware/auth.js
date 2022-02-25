809e4f2e118139f5ee743b6cb279adf7f508e41e
Mankirat <mankirat95@gmail.com>
2022-02-25 23:31:41 +0530

    Auth middleware added

---
+++ src/middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header("Authorization").replace("Bearer ", "");
+        const decodeToken = jwt.verify(token, "AS$%DF)(gh&^");
+        const user = await User.findOne({
+            _id: decodeToken._id,
+            "tokens.token": token,
+        });
+        if (!user) {
+            throw new Error("Unable to Authenicate");
+        }
+        // console.log(user);
+        req.user = user;
+        next();
+    } catch (err) {
+        res.status(401).send({
+            status: 401,
+            message: "Please Authenticate.",
+        });
+    }
+};
+
+module.exports = auth;
